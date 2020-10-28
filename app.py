e55d25ba359037b4d863e244d26166c731514c47
danikrs <danikrs8@gmail.com>
2020-10-28 20:36:41 +0000

    added recipes route

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 import os
 from flask import (
     Flask, render_template, redirect, flash, url_for, request, session)
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 """ I copied the code to set up flask from line 1 to 15
 & the last line from the 2020 task manager mini project videos """
 
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/recipes")
+def recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
