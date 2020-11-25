f9bb00e2f762e2c173ceeecf10fbc4bd96379f33
danikrs <danikrs8@gmail.com>
2020-11-25 22:01:35 +0000

    converted all recipes find to a list, added comments to code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+""" I have used Tim's Task Manager videos a lot as
+a reference throughout my whole project
+and for making the code in this 'app.py' file """
+
 import os
 from flask import (
-    Flask, render_template, redirect, flash, url_for, request)
+    Flask, render_template, flash, url_for, request)
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
@@ -15,9 +19,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-"""I copied most of the code above to set up flask from line 1 to 16
-& the last "if__name__" code from Tims 2020 Task Manager videos """
-
 
 @app.route("/")
 @app.route("/start")
@@ -25,26 +26,30 @@ def start():
     return render_template("start.html")
 
 
+# To display all recipes in All Recipes page
 @app.route("/all_recipes")
 def all_recipes():
-    all_recipes = mongo.db.recipes.find()
+    all_recipes = list(mongo.db.recipes.find())
     return render_template("all_recipes.html", all_recipes=all_recipes)
 
 
+# To get search results in All Recipes page
 @app.route("/search", methods=["GET", "POST"])
 def search():
     index_search = request.form.get("index_search")
-    all_recipes = mongo.db.recipes.find({
-        "$text": {"$search": index_search}})
+    all_recipes = list(mongo.db.recipes.find({
+        "$text": {"$search": index_search}}))
     return render_template("all_recipes.html", all_recipes=all_recipes)
 
 
+# To view the full recipe information
 @app.route("/recipes/<id_for_recipe>")
 def recipes(id_for_recipe):
     recipe = mongo.db.recipes.find_one({"_id":  ObjectId(id_for_recipe)})
     return render_template("recipe_detail.html", recipe=recipe)
 
 
+# In the menu, BREAKFAST
 @app.route("/breakfast")
 def breakfast():
     categories = list(mongo.db.recipes.find({"category": "BREAKFAST"}))
@@ -53,6 +58,7 @@ def breakfast():
         "category.html", categories=categories, category_name=category_name)
 
 
+# In the menu, MEALS
 @app.route("/meals")
 def meals():
     categories = list(mongo.db.recipes.find({"category": "MEALS"}))
@@ -61,6 +67,7 @@ def meals():
         "category.html", categories=categories, category_name=category_name)
 
 
+# In the menu, DESSERTS
 @app.route("/desserts")
 def desserts():
     categories = list(mongo.db.recipes.find({"category": "DESSERTS"}))
@@ -69,6 +76,7 @@ def desserts():
         "category.html", categories=categories, category_name=category_name)
 
 
+# In the menu, SMOOTHIES
 @app.route("/smoothies")
 def smoothies():
     categories = list(mongo.db.recipes.find({"category": "SMOOTHIES"}))
@@ -77,6 +85,7 @@ def smoothies():
         "category.html", categories=categories, category_name=category_name)
 
 
+# For Adding a new Recipe
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
@@ -99,8 +108,7 @@ def add_recipe():
     return render_template("add_recipe.html")
 
 
-# I used Tims Task Manager videos as reference for setting up
-# the code to update a recipe and customized it for my own needs
+# For Updating a Recipe
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     if request.method == "POST":
@@ -124,6 +132,7 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe)
 
 
+# For Deleting a Reicpe
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
